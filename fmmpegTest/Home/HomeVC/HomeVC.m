a0d00d45fa30e8855d29f9924250c7f902392654
sogou <815319775@qq.com>
2016-05-25 18:06:00 +0800

    添加播放功能

--- fmmpegTest/Home/HomeVC/HomeVC.m
+++ fmmpegTest/Home/HomeVC/HomeVC.m
@@ -7,10 +7,14 @@
 //
 
 #import "HomeVC.h"
+#import "KxMovieViewController.h"
+
 
 @interface HomeVC()<UITableViewDelegate , UITableViewDataSource>
 @property (nonatomic , strong) UITableView *mHomeTableView;
 @property (nonatomic , strong) NSArray *contentArray;
+
+@property (nonatomic , strong) NSString *basePatch;
 
 @end
 @implementation HomeVC
@@ -21,10 +25,22 @@
     
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *docDir = [paths objectAtIndex:0];
+    _basePatch = docDir;
     NSLog(@"%@",docDir);
     
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSError *error = nil;
+    NSArray *fileList = [[NSArray alloc] init];
+    //fileList便是包含有该文件夹下所有文件的文件名及文件夹名的数组
+    fileList = [fileManager contentsOfDirectoryAtPath:docDir error:&error];
+    _contentArray = fileList;
     
-    _mHomeTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+   
+    
+    NSLog(@"路径==%@,fileList%@",docDir,fileList);
+
+    
+    _mHomeTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 20, self.view.frame.size.width, self.view.frame.size.height - 20) style:UITableViewStylePlain];
     _mHomeTableView.dataSource = self;
     _mHomeTableView.delegate = self;
     [self.view addSubview:_mHomeTableView];
@@ -41,9 +57,35 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return nil;
+    static NSString *cellIdentifier = @"cellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+        
+    }
+    
+    NSString *name = [_contentArray objectAtIndex:indexPath.row];
+    cell.textLabel.text = name;
+    
+    return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"ffffffff");
+    NSString *filepatch = [NSString stringWithFormat:@"%@/%@",_basePatch,_contentArray[indexPath.row]];
+    
+    NSMutableDictionary *parameters = [NSMutableDictionary dictionary];
+    
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone)
+        parameters[KxMovieParameterDisableDeinterlacing] = @(YES);
+
+    KxMovieViewController *vc = [KxMovieViewController movieViewControllerWithContentPath:filepatch
+                                                                               parameters:parameters];
+    [self presentViewController:vc animated:YES completion:nil];
+
+}
 
 
 @end
